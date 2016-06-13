01450f4b12c79924c9077619f3b63fd2e95a9702
rowieej <rowena.jaico@gmail.com>
2016-06-13 17:21:47 -0400

    added title

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,18 +13,26 @@ var userData = connectedData.push();
 
 $(document).on("click", "#submitMe", function(){
 	console.log("test");
-
+//retrieve user input and set to a variable
 	train = $("#train").val();
 	destination = $("#destination").val();
 	time = $("#trainTime").val();
 	frequency = $("#frequency").val();
-
+//push information up to firebase
 		trainData.push({
 		train: train,
 		destination: destination,
 		time: time,
 		frequency: frequency
+//don't refresh the page
+		});
+		return false;
+
 	});
+
+
+
+//set the information
 
 	
 
